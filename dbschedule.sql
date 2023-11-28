d200e88209ea0f7640759144c28abf082a26b32f
Arkeo <arkeoj@gmail.com>
2023-11-28 23:55:08 +0800

    update

--- dbschedule.sql
+++ dbschedule.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2023 at 05:27 PM
+-- Generation Time: Nov 28, 2023 at 04:54 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -34,6 +34,27 @@ CREATE TABLE `tblsubjects` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `tblsubjects`
+--
+
+INSERT INTO `tblsubjects` (`Subject_ID`, `Subject_Code`, `Subject_Name`) VALUES
+(15, 0, 'Math'),
+(16, 11, 'pol');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsyllabus`
+--
+
+CREATE TABLE `tblsyllabus` (
+  `syllabus_id` int(11) NOT NULL,
+  `subject_id` int(255) NOT NULL,
+  `syllabus_code` varchar(255) NOT NULL,
+  `syllabus_author` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
 -- Indexes for dumped tables
 --
 
@@ -44,6 +65,13 @@ ALTER TABLE `tblsubjects`
   ADD PRIMARY KEY (`Subject_ID`);
 
 --
+-- Indexes for table `tblsyllabus`
+--
+ALTER TABLE `tblsyllabus`
+  ADD PRIMARY KEY (`syllabus_id`),
+  ADD KEY `fksubject_id` (`subject_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -51,7 +79,23 @@ ALTER TABLE `tblsubjects`
 -- AUTO_INCREMENT for table `tblsubjects`
 --
 ALTER TABLE `tblsubjects`
-  MODIFY `Subject_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `Subject_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `tblsyllabus`
+--
+ALTER TABLE `tblsyllabus`
+  MODIFY `syllabus_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tblsyllabus`
+--
+ALTER TABLE `tblsyllabus`
+  ADD CONSTRAINT `fksubject_id` FOREIGN KEY (`subject_id`) REFERENCES `tblsubjects` (`Subject_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
