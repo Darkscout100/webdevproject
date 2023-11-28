3ca3762eb0bc3713dafd80ef820fddda9848602d
Arkeo <arkeoj@gmail.com>
2023-11-28 22:55:08 +0800

    Update

--- forms/addsyllabusform.php
+++ forms/addsyllabusform.php
@@ -5,35 +5,46 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Syllabus</title>
+        <title>Add Syllabus</title>
+        
     </head>
     <body>
         <h1>My subjects</h1>
-        <a href="forms/addform.php">Add Subjects</a><br/><br/>
-        <table width="100%">
-            <tr>
-                <td>ID</td>
-                <td>Subject Code</td>
-                <td>Subject Name</td>
-                <td>Syllabus ID</td>
+        <a href="../index.php"></a>
+        <br/><br/>
+        <form action="../funtions/addsyllabus.php" method="post" name="form1">
+        <table width="25%">
+            <tr>   
                 <td>Syllabus Code</td>
-                <td>Syllabus Author</td>
-                <td>Action</td>
+                <td><input type="text" name="code"></td>
             </tr>
-            <?php
-            while($res = mysqli_fetch_array($result)) {
-                echo "<tr>";
-                echo "<td>".$res['Subject_ID']."</td>";
-                echo "<td>".$res['Subject_Code']."</td>";
-                echo "<td>".$res['Subject_Name']."</td>";
-                echo "<td>".$res['Syllabus_id']."</td>";
-                echo "<td>".$res['Syllabus_code']."</td>";
-                echo "<td>".$res['Syllabus_author']."</td>";
-                echo "<td><a href=\"forms/editform.php?id=$res[Subject_ID]\">Edit</a> |
-                        <a href=\"functions/delete.php?id=$res[Subject_ID]\"
-                        onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
-            }
-            ?>
-        </table>
+            <tr>   
+                <td>Syllabus Author</td>
+                <td><input type="text" name="author"></td>
+            </tr>
+            <tr>   
+                <td>Subject</td>
+                <td>
+                    <select name="subject">
+                        <?php 
+                            include_once("../dbConnection/mysqlconfig_connection.php");
+                            $query = "SELECT * FROM tblsubjects"; 
+                            $result = mysqli_query($dbc,$query);
+                            while($res = mysqli_fetch_array($result)) {
+                                echo "<option value=\"".$res['Subject_ID']."\">";
+                                echo $res['Subject_Name']."</td>";
+                                echo "</option>";
+                            }
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type='submit' name='submit' value='Add'></td>
+            </tr>
+
+            </table>
+        </form>
     </body>
 </html>
